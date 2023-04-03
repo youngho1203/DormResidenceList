7e40f17321c8939d2cffc6157934b059a5fc6e85
Youngho Cho <youngho1203@gmail.com>
2023-04-03 17:46:26 +0900

    remove select method, use filter function instead

--- Code.js
+++ Code.js
@@ -1,53 +1,9 @@
 const ws = SpreadsheetApp.getActiveSpreadsheet();
 const listsSheet = ws.getSheetByName("현황");
-const CONFIG_SHEET_NAME = 'Config';
-const configSheet = ws.getSheetByName(CONFIG_SHEET_NAME);
 // 생년월일 Column
 const BIRTH_DAY_COLUMN = 7;
 // CheckOut Column
 const CHECK_OUT_COLUMN = 4;
-
-/** 
- * Creates the menu items for user to run scripts on drop-down.
- */
-function onOpen() {
-  const ui = SpreadsheetApp.getUi();
-  ui.createMenu('Gather Data')
-      .addItem('Select EmailAddress', 'selectEmailAddress')
-      .addToUi();
-}
-
-function selectEmailAddress() { 
-  var emailAddress = [];
-  var last_low = listsSheet.getLastRow();
-  listsSheet.getRange("D3:E" + last_low).getValues().forEach((value, index) => { if(!value[0]) {
-    var address = listsSheet.getRange("Q" + (3 + index)).getValue();
-    if(address !== '') {
-      const studentName = listsSheet.getRange("F" + (3 + index)).getValue();
-      const emailCellData = [listsSheet.getRange("Q" + (3 + index)).getBackground(), value[1], studentName, address ];
-      emailAddress.push(emailCellData);
-    }
-  }});
-  addEmailAddressSheet(emailAddress);
-}
-
-const sheetName = "ActiveEmailAddress";
-
-function addEmailAddressSheet(emailAddress) {
-  // 
-  var emailSheet = ws.getSheetByName(sheetName);
-  if(!emailSheet) {
-    ws.insertSheet(sheetName);
-  }
-  emailSheet = ws.getSheetByName(sheetName);
-  var lastColumn = emailSheet.getLastColumn() + 1;
-  lastColumn++;
-  emailAddress.forEach((a, index) => emailSheet.getRange(1 + index, lastColumn).setValue(a[1]).setBackground(a[0]));
-  lastColumn++;
-  emailAddress.forEach((a, index) => emailSheet.getRange(1 + index, lastColumn).setValue(a[2]).setBackground(a[0]));
-  lastColumn++;
-  emailAddress.forEach((a, index) => emailSheet.getRange(1 + index, lastColumn).setValue(a[3]).setBackground(a[0]));  
-}
 
 // Regular expression to check if string is valid date
 const DATE_PATTERN = /^(\d{4})(-|\/|\. )(0?[1-9]|1[012])(-|\/|\. )(0?[1-9]|[12][0-9]|3[01])$/;
@@ -78,22 +34,6 @@ function onEdit(e) {
   _range.setTextStyle(style_builder.setStrikethrough(range_modified.getValue()).build());
   // has_extension column
   _extension_cell.setTextStyle(cell_text_style);
-}
-
-/**
- * 이 빠진 row 를 찾는다.
- */
-function findEmptyRow(preDefinedArray) {
-  lastRow = listsSheet.getLastRow();
-  listsSheet.getRange("A3:E" + lastRow).getValues().forEach(values, index => {
-    if(isCellEmpty(values[4])) {
-
-    }
-  });
-}
-
-function moveRow(fromRow, toRow) {
-
 }
 
 // Returns true if the cell where cellData was read from is empty.
